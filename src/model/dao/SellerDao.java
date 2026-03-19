40288fda2b0671c6f5f4eb5e4634c98acc6f35d2
AndersonnTech <anderson2csilva98@gmail.com>
2026-03-19 16:37:12 -0300

    insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,8 +7,8 @@ import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void insert(Seller dep);
-	void update(Seller dep);
+	void insert(Seller obj);
+	void update(Seller obj);
 	void deleteById(Integer id);
 	Seller findById(Integer id);
 	List<Seller> findAll();
